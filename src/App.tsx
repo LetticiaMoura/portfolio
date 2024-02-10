d920bc500d630ff6bd765e2943cdbe72867f434d
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-02-10 13:02:44 -0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Home from './components/pages/Home';
 import Skills from './components/pages/Skills';
 import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
-import Footer from './components/layout/Footer';
-
-import './App.css'
+import Footer from './components/layout/Footer';5
 
 function App() {
 
