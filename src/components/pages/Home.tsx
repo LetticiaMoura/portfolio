e235bb117c214db8ea7b9eb68f25c0f5030f02a2
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-02-10 10:15:15 -0300

    feat: Home responsivo

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,25 +3,25 @@ import Curriculum from '../layout/Curriculum';
 
 import Perfil from '../../assets/image.jpg';
 
-import Styles from './Home.module.css';
+//import Styles from './Home.module.css';
 
 function Home() {
 
     return(
 
-        <div className={Styles.container}>
+        <div className="flex flex-col-reverse space-y-20 md:space-y-0 xl:flex-row items-center md:justify-around p-8 bg-pessego md:h-screen">
 
-            <div>
+            <div className='h-[40vh]'>
 
-                <h1>Hello, my name is <br></br> <span> Leticia Moura </span> </h1>
+                <h1 className="py-2.5 text-3xl md:text-4xl font-mono text-center md:text-start font-gfs-didot">Hello, my name is <br></br> <span className="font-gfs-didot text-orange text-start text-5xl md:text-7xl font-mono"> Leticia Moura </span> </h1>
 
-                <p>I'am Frontend Developer</p>
+                <p className="text-2xl md:text-2xl font-mono text-center  md:text-start font-gfs-didot">I'am Frontend Developer</p>
 
-                <button> <Curriculum /> </button>
+                <button className='m-5 p-2 rounded-md hover:text-orange hover:bg-slate-gray bg-slate-800 text-center bg-slate-blue'> <Curriculum /> </button>
                 
             </div>
 
-           <img className={Styles.perfil} src={Perfil} alt='Profile Photo'/>
+           <img className="pt-1/5 h-2/6 md:h-96 rounded-full" src={Perfil} alt='Profile Photo'/>
 
         </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'gfs-didot': ['GFS Didot', 'serif'],
+    },
+    colors: {
+      'pessego': '#ffdab9',
+      'orange': '#f97316',
+      'slate-blue': '#020617',
+      'slate-gray': '#0f172a',
+      
+      },
   },
   plugins: [],
 }
