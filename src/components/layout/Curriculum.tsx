6c402902fab442d07033a47cb215f93b31d4484b
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-02-10 10:43:02 -0300

    refactor: Deletando Curriculum.module.css

--- src/components/layout/Curriculum.tsx
+++ src/components/layout/Curriculum.tsx
@@ -2,8 +2,6 @@ import  { Component } from 'react';
 
 import curriculum from '../../assets/curriculoLM.pdf';
 
-//import Styles from './Curriculum.module.css';
-
 class Curriculum extends Component {
 
   render() {
