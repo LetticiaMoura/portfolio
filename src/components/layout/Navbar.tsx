87e0dea933cb0314b7abc10c514b22aabd07ab76
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-02-19 16:25:28 -0300

    style: Ajuste na li da navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -23,15 +23,15 @@ function Navbar() {
 
     return(
 
-        <nav className="py-8 flex flex-wrap items-center justify-evenly p-2 h-20 bg-pessego">
+        <nav className="py-8 flex flex-wrap items-center justify-around p-2 h-20 bg-pessego">
 
             <h1 className="font-gfs-didot text-2xl font-bold opacity-90">LM <span className="text-orange"> | </span> PORTFOLIO</h1>
 
-            <ul className="md:flex xl:flex justify-evenly list-none text-center text-slate-950 hidden">
+            <ul className="md:flex justify-around list-none text-center text-slate-950 hidden">
 
                 {menuItems.map(menu => (
 
-                    <li className="font-gfs-didot opacity-90"> <a className="font-medium hover:underline hover:underline-offset-4 text-2xl	" href={ `#${menu.title}` } > { menu.title } </a> </li>
+                    <li className="font-gfs-didot opacity-90 mr-10"> <a className="font-medium hover:underline hover:underline-offset-4 text-2xl	" href={ `#${menu.title}` } > { menu.title } </a> </li>
 
                 ))}
 
