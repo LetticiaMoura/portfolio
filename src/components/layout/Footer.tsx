d66e01bc732add56c202b07cbbf7578970d0eb21
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-02-19 16:21:02 -0300

    feat: Footer responsivo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,35 +1,33 @@
 
 import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 
-import Styles from './Footer.module.css';
-
 function Footer() {
 
     return(
 
-        <footer className={Styles.footer}>
+        <footer className="bg-pessego text-slate-white p-8 text-center relative left-0 bottom-0">
 
-            <ul>
+            <ul className="flex  justify-center list-none">
 
                 <li>
-                    <a href='https://github.com/LetticiaMoura'> <FaGithub /> </a>
+                    <a className="text-slate-white text-3xl hover:text-orange mr-16 text-center" href='https://github.com/LetticiaMoura'> <FaGithub /> </a>
                 </li>
 
                 <li>
                     
-                    <a href='https://www.linkedin.com/in/leticia-moura-2157891a6'> <FaLinkedin /> </a>
+                    <a className="text-slate-white text-3xl hover:text-orange mr-16 text-center" href='https://www.linkedin.com/in/leticia-moura-2157891a6'> <FaLinkedin /> </a>
 
                 </li>
 
                 <li>
 
-                    <a href='https://www.instagram.com/leticiamourah_?igsh=aGpoZHkzYWx2cDRy'> <FaInstagram /> </a>
+                    <a className="text-slate-white text-3xl hover:text-orange mr-12 text-center" href='https://www.instagram.com/leticiamourah_?igsh=aGpoZHkzYWx2cDRy'> <FaInstagram /> </a>
 
                 </li>
                 
             </ul>
 
-            <p className={Styles.copy_right} > <span> Leticia Moura </span> &copy; 2024</p>
+            <p className="mt-2 text-2xl"> <span className="font-weight"> Leticia Moura </span> <span className=" text-orange"> &copy; </span>2024</p>
 
         </footer>
             
