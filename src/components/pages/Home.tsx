5e434e18e7d34daee28d0d4380447837c897f4b4
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-03-02 19:11:49 -0300

    feat:  Imagem responsiva

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -13,13 +13,13 @@ function Home() {
 
                 <h1 className="py-2 text-3xl md:text-4xl font-mono text-center md:text-start font-gfs-didot">Hello, my name is <br></br> <span className="font-gfs-didot text-orange text-start text-5xl md:text-7xl font-mono">Leticia Moura </span> </h1>
 
-                <p className="text-2xl md:text-2xl font-mono text-center lg:text-start font-gfs-didot">I'am Frontend Developer</p>
+                <p className="text-2xl md:text-2xl md:text-start font-mono text-center lg:text-start font-gfs-didot">I'am Frontend Developer</p>
 
                 <button className='p-2 mt-3 text-slate-blue font-bold rounded-md border-2 border-solid border-slate-blue hover:text-orange hover:bg-slate-gray/80 hover:border-none text-center'> <Curriculum /> </button>
                 
             </div>
 
-           <img className="rounded-s-full pt-1/5 shadow-lg shadow-slate-blue/50 h-2/6 md:h-96 md:rounded-full" src={Perfil} alt='Profile Photo'/>
+           <img className="rounded-e-full sm:w-[80vw] md:w-[100%] pt-1/5 shadow-lg shadow-slate-blue/50 h-2/6 md:h-[50%] lg:w-auto lg:h-[70%]  sm:rounded-full md:rounded-full lg:rounded-full xl:rounded-full" src={Perfil} alt='Profile Photo'/>
 
         </div>
 
