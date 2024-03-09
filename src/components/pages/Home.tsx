3d9325bba94c60662829c443393e48c29b36b2a3
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-03-09 10:44:48 -0300

    style

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -18,12 +18,15 @@ function Home() {
                 <button className='p-2 mt-3 text-slate-blue font-bold rounded-md border-2 border-solid border-slate-blue hover:bg-slate-gray/30 text-center'> <Curriculum /> </button>
                 
             </div>
-            
-            <div className="h-[45vh] w-[80vw] border-orange border-[15px] sm:border-[15px] pq:h-[48vh] pq:w-[60vw] pq:border-[15px] rounded-full p-2 animate-spin absolute top-[13%] sm:h-[55vh] sm:w-[55vw] md:hidden"></div>
-
-            <div className="h-[50vh] w-[75vw] border-pessego border-[15px] sm:border-[8px] pq:h-[45vh] pq:w-[45vh] pq:border-[8px] rounded-full p-2 animate-spin absolute top-[13%] sm:h-[55vh] sm:w-[57vw] md:hidden"></div>
 
             <img className="w-auto h-[40vh] min-h-auto min-w-auto rounded-full sm:w-auto sm:h-[50vh] md:w-auto shadow-lg shadow-slate-blue/50 md:h-[50%] lg:w-auto lg:h-[70%]" src={Perfil} alt='Profile Photo'/>
+            
+            <div className="h-[43vh] w-[80vw] border-orange border-[15px] sm:border-[20px] pq:top-[16%] pq:h-[40vh] pq:w-[60vw] pq:border-[15px] rounded-full p-2 animate-spin absolute top-[14%] sm:h-[52vh] sm:w-[55vw] md:hidden animation-slow"></div>
+
+            <div className="h-[43vh] w-[80vw] border-orange border-[15px] sm:border-[14px] pq:top-[16%] pq:h-[40vh] pq:w-[55vw] pq:border-[15px] rounded-full p-2 animate-spin absolute top-[14%] sm:hidden sm:h-[55vh] 
+            sm:w-[55vw] md:hidden  animation-slow"></div>
+
+            <div className="h-[50vh] w-[82vw] border-pessego border-[15px] pq:top-[14%] sm:border-[10px] filter brightness-80 pq:h-[42vh] pq:w-[45vh] pq:border-[15px] rounded-full p-2 animate-spin absolute top-[10%] sm:h-[55vh] sm:w-[57vw] md:hidden  animation-slow"></div>
 
 
         </div>
