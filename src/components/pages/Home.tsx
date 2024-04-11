3ab3ef1ef8a6f9a1affbba4ecaa57afc97ea8ae1
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-04-10 21:41:56 -0300

    feat: Mudança de layout da pagina Home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -4,7 +4,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
-import Perfil from '../../assets/my.jpeg'
+import Perfil from '../../assets/let.png'
 
 function Home() {
 
@@ -17,12 +17,13 @@ function Home() {
 
     return(
 
-        <div className="scroll-smooth focus:scroll-auto flex flex-col-reverse space-y-20 h-screen md:space-y-0 md:flex-row items-center md:justify-around p-10 bg-pessego md:h-screen">
+        <div className="scroll-smooth focus:scroll-auto flex flex-col-reverse space-y-20 h-screen md:space-y-0 md:flex-row items-center md:justify-around p-10 bg-pessego">
 
-            <div className="h-[40vh] text-center md:text-start overflow-hidden">
+            <div className="h-[50vh] text-center md:text-start overflow-hidden">
 
-                <h1 className="pt-7 py-2 text-3xl md:text-4xl xl:text-5xl xl:text-slate-950 text-center md:text-start font-gfs-didot">Hello, my name is<br></br> <span className="font-gfs-didot text-orange text-start text-5xl md:text-7xl">Leticia Moura</span> </h1>
+                <h1 className="pt-7 py-2 text-3xl md:text-4xl xl:text-4xl xl:text-slate-950 text-center md:text-start font-gfs-didot">Hello, my name is<br></br> <span className="font-gfs-didot text-orange-dark text-start text-5xl md:text-6xl">Leticia Moura</span> </h1>
 
+                <p className="md:w-[35vw] font-gfs-didot font-medium">This is my portfolio with some informations about me. Here you are find my stacks, some projects and my social medias. I use the language React to construct the frontend.</p>
 
                 <p className='text-xl font-medium text-slate-950 font-gfs-didot'>
                     <span>
@@ -34,79 +35,18 @@ function Home() {
                     </span>
                 </p>
 
-                <div className='flex gap-5'>
-                    <button className='p-2 mt-3 w-42 text-slate-blue font-bold rounded-md border-2 border-solid border-slate-blue hover:bg-slate-gray/30 text-center'> <Curriculum /> </button>
-                    <a href="https://github.com/letticiamoura" className='p-2 mt-3 w-40 text-slate-blue font-bold rounded-md border-2 border-solid border-slate-blue hover:bg-slate cc-gray/30 text-center'><GitHubIcon sx={{ marginRight: '0.5rem' }} />GitHub</a>
+                <div className='flex gap-5 p-1'>
+
+                    <button className='p-2 mt-3 w-42 text-slate-blue font-bold rounded-md border-2 border-solid border-slate-blue hover:border-orange-dark hover:bg-orange/10 text-center transition hover:scale-105'> <Curriculum /> </button>
+
+                    <a href="https://github.com/letticiamoura" target="_blank" className='p-2 mt-3 w-40 text-slate-blue font-bold rounded-md border-2 border-solid border-slate-blue hover:border-orange-dark hover:bg-orange/10 text-center transition hover:scale-105'><GitHubIcon sx={{ marginRight: '0.5rem' }} />GitHub</a>
+
                 </div>
                             
             </div>
             
-            <img className="h-[38vh] w-[70vw] object-cover rounded-full z-40 border-[8px] border-pessego/50 pq:h-[40vh] pq:w-[65vw] em:h-[50vh] em:w-auto md:h-[50vh] md:w-auto lg:h-[55vh] lg:w-[40vw] xl:h-[62vh] xl:w-auto 2xl:h-[70vh] 2xl:w-[37vw]" src={Perfil} alt='Profile Photo'/>
-            
-            <div className="
-                h-[40vh] 
-                w-[75vw]  
-                rounded-full
-                absolute 
-                top-[16%] 
-                border-orange 
-                border-[20px]  
+            <img className="h-[38vh] w-[70vw] bg-orange-dark object-cover rounded-t-full rounded-bl-full z-40 pq:h-[40vh] pq:w-[65vw] em:h-[50vh] em:w-auto md:h-[50vh] md:w-auto lg:h-[55vh] lg:w-[40vw] xl:h-[58vh] xl:w-auto 2xl:h-[70vh] 2xl:w-[37vw] shadow-slate-900/50 shadow-xl" src={Perfil} alt='Profile Photo'/>
 
-                pq:top-[16%] 
-                pq:h-[40vh] 
-                pq:w-[70vw] 
-                pq:border-[20px]
-
-                em:top-[15%] 
-                em:h-[51vh] 
-                em:w-[70vw] 
-                em:border-[15px]
-
-                sm:top-[15%] 
-                sm:border-[30px] 
-                sm:h-[57vh] 
-                sm:w-[57vw] 
-
-                md:hidden
-
-                xl:top-[15%] 
-                xl:left-[50%]
-                xl:border-[25px] 
-                xl:h-[80vh] 
-                xl:w-[65vw] 
-
-                animate-spin-slow"
-                >
-            </div>
-
-            <div className="
-                hidden
-                h-[44vh] 
-                w-[79vw]  
-                rounded-full 
-                absolute 
-                top-[14%] 
-                border-pessego
-                border-[8px]  
-
-                pq:top-[15%] 
-                pq:h-[42vh] 
-                pq:w-[70vw] 
-                pq:border-[5px]
-
-                em:top-[15%] 
-                em:border-[5px] 
-                em:h-[42vh] 
-                em:w-[60vw] 
-
-                sm:top-[14.5%] 
-                sm:border-[5px] 
-                sm:h-[54vh] 
-                sm:w-[63vw] 
-
-                md:hidden 
-                animate-spin-fast"
-            ></div>        
 
         </div>
 
