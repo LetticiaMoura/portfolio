7897f85a5289d7dd4b260cd364a1cb9e9e04bb68
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-04-14 15:29:02 -0300

    style: AJustando espaçamento

--- src/components/pages/Skills.tsx
+++ src/components/pages/Skills.tsx
@@ -12,8 +12,8 @@ function Skills() {
             
             <div className="flex flex-wrap p-[10px] lg:p-5 md:p-[40px] justify-center items-center">
 
-                <div className='flex justify-around w-fit lg:w-[70%] p-10 lg:p-5'>
-                    <i className="devicon-html5-plain-wordmark text-pessego text-[4em] md:text-[6em] box-border hover:text-[#ff5c0a] transition hover:-translate-y-2 hover:scale-125 duration-400"></i>
+                <div className='flex justify-around w-fit sm:w-[70%] p-10 lg:p-6'>
+                    <i className="devicon-html5-plain-wordmark text-pessego p-3 text-[4em] md:text-[6em] box-border hover:text-[#ff5c0a] transition hover:-translate-y-2 hover:scale-125 duration-400"></i>
 
                     <i className="devicon-css3-plain-wordmark text-pessego text-[4em] md:text-[6em] hover:text-blue-600 p-[10px] box-border 
                     transition hover:-translate-y-2 hover:scale-125 duration-400"></i>
@@ -22,7 +22,7 @@ function Skills() {
                     transition hover:-translate-y-2 hover:scale-125 duration-400"></i>
                 </div>
 
-                <div className='flex justify-around w-fit lg:w-[70%] p-10 lg:p-5'>
+                <div className='flex justify-around w-fit sm:w-[70%] p-10 lg:p-6'>
                     <i className="devicon-javascript-plain text-pessego text-[4em] md:text-[6em] hover:text-yellow-400 p-[10px] box-border 
                     transition hover:-translate-y-2 hover:scale-125 duration-400"></i>
 
@@ -33,7 +33,7 @@ function Skills() {
                     transition hover:-translate-y-2 hover:scale-125 duration-400"></i>
                 </div>
                 
-                <div className='flex justify-around w-fit lg:w-[70%] p-10 lg:p-5'>
+                <div className='flex justify-around w-fit sm:w-[70%] p-10 lg:p-6'>
                     <i className="devicon-postgresql-plain-wordmark text-pessego text-[4em] md:text-[6em] hover:text-white p-[10px] box-border 
                     transition hover:-translate-y-2 hover:scale-125 duration-400"></i>
 
